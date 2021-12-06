1fb2ef0840e946f82c8cf7f295e4547a578d5de0
carlosoler <c.soler13@usp.ceu.es>
2021-12-06 12:29:42 +0100

    test semi pleno OK

--- Bolera.py
+++ Bolera.py
@@ -2,7 +2,9 @@ class Bolera:
 
     def calcularPuntuacion(self, partida):
         suma = 0
-        for tirada in partida:
-            if tirada != '-':
+        for index, tirada in enumerate(partida):
+            if tirada == '/':
+                suma = suma - int(partida[index-1]) + 10 + int(partida[index+1])
+            elif tirada != '-':
                 suma = suma + int(tirada)
         return suma
