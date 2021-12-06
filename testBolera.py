83fd7eedc08a52046effc579be6efa098a12586b
carlosoler <c.soler13@usp.ceu.es>
2021-12-06 11:47:31 +0100

    test calcular puntuacion de 0 ERROR

--- testBolera.py
+++ testBolera.py
@@ -5,6 +5,12 @@ class MyTestCase(unittest.TestCase):
     def test_iniciarPartida(self):
         bolera = Bolera()
 
+    def test_calculaPuntuacion_0(self):
+        bolera = Bolera()
+        partida = '--------------------'
+        resultado = bolera.calularPuntuacion(partida)
+        self.assertEqual(0, resultado)
+
 
 if __name__ == '__main__':
     unittest.main()
