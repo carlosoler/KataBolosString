3eb0b37912c199d4fcbbe76f5d80aa41cd4a368c
carlosoler <c.soler13@usp.ceu.es>
2021-12-06 11:45:12 +0100

    test iniciar partida ERROR

---
+++ testBolera.py
@@ -0,0 +1,10 @@
+import unittest
+from Bolera import *
+
+class MyTestCase(unittest.TestCase):
+    def test_iniciarPartida(self):
+        bolera = Bolera()
+
+
+if __name__ == '__main__':
+    unittest.main()
